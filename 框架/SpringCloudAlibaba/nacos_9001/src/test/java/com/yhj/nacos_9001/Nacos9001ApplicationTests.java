59c204a343dfc4f197cca7e52d1ac2cc7b2119ab
尹宏君 <18773932621@163.com>
2022-09-21 00:59:58 +0800

    第一次提交框架

---
+++ 框架/SpringCloudAlibaba/nacos_9001/src/test/java/com/yhj/nacos_9001/Nacos9001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhj.nacos_9001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nacos9001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ 框架/SpringCloudAlibaba/nacos_9002/src/test/java/com/yhj/nacos_9002/Nacos9002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhj.nacos_9002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nacos9002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

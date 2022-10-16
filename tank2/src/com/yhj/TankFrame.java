88c1d83da128da67b32f9c306876ab827f6b862c
尹宏君 <18773932621@163.com>
2022-10-16 22:58:47 +0800

    新建一个TankFrame类继承Frame类，重写它的paint方法
    Signed-off-by: 尹宏君 <18773932621@163.com>

---
+++ tank2/src/com/yhj/TankFrame.java
@@ -0,0 +1,29 @@
+package com.yhj;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+
+    public TankFrame(){
+        setSize(800,800);
+        setResizable(false);
+        setTitle("Tank war");
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g){
+        g.fillRect(200,200,50,50);
+    }
+
+
+}

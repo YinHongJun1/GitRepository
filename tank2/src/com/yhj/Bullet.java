e3afcefe6a694dd3ce1a57280aeab013d6ead653
YinHongJun1 <18773932621@163.com>
2022-11-02 07:59:25 +0800

    优化敌方坦克爆炸的范围和位置 Signed-off-by: 尹宏君 <18773932621@163.com> Signed-off-by: YinHongJun1 <18773932621@163.com>
    Signed-off-by: YinHongJun1 <18773932621@163.com>

--- tank2/src/com/yhj/Bullet.java
+++ tank2/src/com/yhj/Bullet.java
@@ -89,7 +89,9 @@ public class Bullet {
         if (rect.intersects(rect2)){
                 tank.die();
                 this.die();
-                tf.explodes.add(new Explode(x,y,tf));
+                int ex = tank.getX()+Tank.WIDTH/2-Explode.WIDTH/2;
+                int ey = tank.getY()+Tank.HEIGHT/2-Explode.HEIGHT/2;
+                tf.explodes.add(new Explode(ex,ey,tf));
         }
     }
 
